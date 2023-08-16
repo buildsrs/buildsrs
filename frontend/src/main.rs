72729d40dbe3f0d8e6be1f5d2c701dcb064f2fbf
Patrick Elsen <pelsen@xfbs.net>
2023-08-17 00:26:47 +0200

    Adds boilerplate frontend code

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,42 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div class="flex flex-col min-h-screen">
+            <header class="bg-yellow-600">
+                <div class="max-w-4xl mx-auto p-4">
+                    <nav class="flex items-center gap-2">
+                        <img src="/static/builder.svg" class="w-8 h-8" />
+                        <span class="text-3xl font-bold">{"builds.rs"}</span>
+                        <span class="grow"></span>
+                        <span>{"Browse all crates"}</span>
+                    </nav>
+                    <div class="text-center text-4xl font-bold my-6">
+                        {"Build service for Rust crates on crates.io"}
+                    </div>
+                    <div class="text-center my-6">
+                        <input type="text" class="rounded-xl w-[70%] p-2 px-4" />
+                    </div>
+                </div>
+            </header>
+            <main class="bg-amber-200 grow">
+                <div class="max-w-4xl mx-auto p-4">
+                    {"Content"}
+                </div>
+            </main>
+            <footer class="bg-yellow-600">
+                <div class="max-w-4xl mx-auto p-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+                    <div>{"Rust"}</div>
+                    <div>{"Get help"}</div>
+                    <div>{"Policies"}</div>
+                    <div>{"Social"}</div>
+                </div>
+            </footer>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ["src/**/*.{html,js,rs,css,rs}", "index.html"],
+    plugins: [
+        require('@tailwindcss/typography')
+    ]
+}
